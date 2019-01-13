fc52bb8ba3e0db09742e345b72a50d2768c3a745
teaching-innovation <Joel.l.phillips@gmail.com>
2019-01-13 13:19:35 +1030

    Create output from izoneTest2.py

---
+++ custom_components/climate/Testing/output from izoneTest2.py
@@ -0,0 +1,40 @@
+ACError:  OK
+FreeAir: disabled
+DeviceType: ASH
+OemMake: 0
+UnitType: Panasonic
+Warnings: none
+Id: 0
+UnitLocked: false
+AirflowLock: off
+NoOfConst: 1
+SysType: 320
+Tag1: ROEBUCK
+SysOn: on
+SleepTimer: 0
+Setpoint: 26.0
+EcoMax: 30.0
+Temp: 0.0
+AirStreamDeviceUId: 000018279
+NoOfZones: 5
+FanAuto: disabled
+SysFan: low
+Supply: 0.0
+SysMode: cool
+EcoMin: 15.0
+RAS: RAS
+Tag2: PHILLIPS
+CtrlZone: 15
+EcoLock: true
+26.0
+Zone Info:
+MASTER:open
+BABY:close
+MUSIC:open
+GUEST:close
+LIVING:open
+Zone 6:open
+Zone 7:open
+Zone 8:open
+['open', 'close', 'open', 'close', 'open', 'open', 'open', 'open']
+close
